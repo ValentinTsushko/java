e4ec9d0749b52f73c05fa353fde1b5ae901d76de
Valentin_Tsushko <35625439+vladwolf@users.noreply.github.com>
2021-01-13 18:58:39 +0200

    Update main.java

--- мои проэкты/calculator/main/main.java
+++ мои проэкты/calculator/main/main.java
@@ -15,57 +15,16 @@ public class main{
 			
 			String start = in.nextLine();
 			
-			if(start.equals("+")){
-				Plus p = new Plus();
-				while(true){
-					System.out.println("Okey, what're we doing now?");
-					System.out.println("change first number (1), change second number (2),");
-					System.out.println("plus(3), view first (4), view second (5),");
-					System.out.println("view sum (6), view all operations (7), let's start anew (0)");
-
-					start = in.nextLine();
-					
-					double x = 0;
-					if(start.equals("0")) {
-						System.out.println("");
-						break;
-					}
-					else if(start.equals("1")) {
-						x = in.nextDouble();
-						p.plus_take_a(x);
-						System.out.println("");
-					}
-					else if(start.equals("2")) {
-						x = in.nextDouble();
-						p.plus_take_b(x);
-						System.out.println("");
-					}
-					else if(start.equals("3")) {
-						p.plus();
-						double tmp = p.plus_get_c();
-						last_operation.add_last((double)tmp);
-						System.out.println(tmp);
-						System.out.println("");
-					}
-					else if(start.equals("4")) {
-						System.out.println(p.plus_get_a());
-						System.out.println("");
-					}
-					else if(start.equals("5")) {
-						System.out.println(p.plus_get_b());
-						System.out.println("");
-					}
-					else if(start.equals("6")) {
-						System.out.println(p.plus_get_c());
-						System.out.println("");
-					}
-					else if(start.equals("7")){
-						last_operation.view();
-					}
-				}
-			}
+			switch(start){
+				case("+"):
+					Plus_menu pm = new Plus_menu();
+					pm.start_Plus(last_operation);
+					break;
 			
-			else if(start.equals("0")) System.exit(0);
+				case("0"):
+					System.exit(0);
+					break;
+			}
 		}
 	}
 }
